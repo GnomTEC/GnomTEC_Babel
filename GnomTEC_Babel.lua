94aa7cd86212fa91a766dbf1396702dd12f21aee
Effie Flammfix <github@gnomtec.de>
2022-03-12 19:56:08 +0100

    * Updated TOC for WoW patch 9.2.0

--- GnomTEC_Babel.lua
+++ GnomTEC_Babel.lua
@@ -1,10 +1,10 @@
 ﻿-- **********************************************************************
 -- GnomTEC Babel
--- Version: 9.1.5.30
+-- Version: 9.2.0.30
 -- Author: Peter Jack
 -- URL: http://www.gnomtec.de/
 -- **********************************************************************
--- Copyright © 2011-2021 by Peter Jack
+-- Copyright © 2011-2022 by Peter Jack
 --
 -- Licensed under the EUPL, Version 1.1 only (the "Licence");
 -- You may not use this work except in compliance with the Licence.
@@ -37,17 +37,17 @@ GnomTEC_Babel_Options = {
 -- ----------------------------------------------------------------------
 
 -- internal used version number since WoW only updates from TOC on game start
-local addonVersion = "9.1.5.30"
+local addonVersion = "9.2.0.30"
 
 -- addonInfo for addon registration to GnomTEC API
 local addonInfo = {
 	["Name"] = "GnomTEC Babel",
 	["Version"] = addonVersion,
-	["Date"] = "2021-07-01",
+	["Date"] = "2022-03-12",
 	["Author"] = "Peter Jack",
 	["Email"] = "info@gnomtec.de",
 	["Website"] = "http://www.gnomtec.de/",
-	["Copyright"] = "(c)2011-2021 by Peter Jack",
+	["Copyright"] = "(c)2011-2022 by Peter Jack",
 }
 
 -- GnomTEC API revision
